56f4fd4744ccfbe4e608ce5a96ede73e4d0ed419
Antonio Terreno <antonio.terreno@gmail.com>
2014-07-21 13:25:30 +0100

    time to rewrite the whole thing in node.js

--- resources/public/main.js
+++ resources/public/main.js
@@ -1,6 +1,11 @@
-var findSong = function(song) {
-    console.log(song["@name"]);
-
+var findTrack = function(song, artistName) {
+    $.ajax({
+	url: 'https://api.spotify.com/v1/search',
+	data: { q: artistName + " " + song["@name"], type: "track", limit: 1},
+	success: function (response) {
+	    console.log(_.first(response.tracks.items));
+	}
+    });
 }
 
 var searchSetList = function (artistName, date) {
@@ -8,7 +13,7 @@ var searchSetList = function (artistName, date) {
 	url: '/search/' + artistName + '/' + date,
 
 	success: function (response) {
-	    _.each(response.setlists.setlist.sets.set, function(set) { _.each(set.song, findSong); });
+	    _.each(response.setlists.setlist.sets.set, function(set) { _.each(set.song, function(song) {findTrack(song, artistName)}); });
 	}
     });
 };
